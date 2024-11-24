bc386bfcfe0902884649f45e48b92e9db96e6c19
Sambo Thach <sambo.thach@pm.me>
2024-11-24 18:56:03 -0500

    add check to see if image is the latest. Only run compose if the container is the latest.

--- start_obsidian.sh
+++ start_obsidian.sh
@@ -11,6 +11,22 @@ PGID=$(id -g)
 chown -R $PUID:$PGID ~/docker_data/obsidian/config
 chown -R $PUID:$PGID ~/docker_data/obsidian/vaults
 
-# Pull the latest image and start the Obsidian container
-docker-compose pull
-docker-compose up -d
+# Check if the container is already running
+if [ "$(docker ps -q -f name=obsidian)" ]; then
+    echo "Success: The Obsidian container is already running."
+else
+    # Check if the local image is up to date
+    LOCAL_DIGEST=$(docker image inspect lscr.io/linuxserver/obsidian:latest --format '{{.RepoDigests}}')
+    REMOTE_DIGEST=$(docker pull --quiet lscr.io/linuxserver/obsidian:latest | tail -n 1)
+
+    if [ "$LOCAL_DIGEST" != "$REMOTE_DIGEST" ]; then
+        echo "Updating to the latest image..."
+        docker-compose pull
+    else
+        echo "You already have the latest image."
+    fi
+
+    # Start the container
+    docker-compose up -d
+    echo "Obsidian container has been started."
+fi
